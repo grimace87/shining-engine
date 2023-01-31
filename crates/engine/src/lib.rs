7e7d20ea4999b2bf52f69fb1a72ac5f3c0b1dae6
Thomas Reichert <thomasr1987@gmail.com>
2023-02-01 06:05:32 +1100

    Moved internals out of engine struct, simplifying code

--- crates/engine/src/lib.rs
+++ crates/engine/src/lib.rs
@@ -27,8 +27,7 @@ use std::fmt::Debug;
 
 pub struct Engine<M: 'static + Send + Debug> {
     app_title: &'static str,
-    looper: Option<WindowEventLooper<M>>,
-    internals: Option<EngineInternals>
+    looper: Option<WindowEventLooper<M>>
 }
 
 impl<M: 'static + Send + Debug> Engine<M> {
@@ -36,8 +35,7 @@ impl<M: 'static + Send + Debug> Engine<M> {
     pub fn new(app_title: &'static str) -> Self {
         Self {
             app_title,
-            looper: Some(WindowEventLooper::new()),
-            internals: None
+            looper: Some(WindowEventLooper::new())
         }
     }
 
@@ -67,6 +65,7 @@ impl<M: 'static + Send + Debug> Engine<M> {
         let Some(looper) = self.looper.take() else {
             panic!("Internal error");
         };
+        let mut internals = EngineInternals::new(&window, &app).unwrap();
         let running_window_id = window.get_window_id();
         app.on_window_state_event(WindowStateEvent::Starting);
         let code = looper.run_loop(move |event, _, control_flow| {
@@ -75,9 +74,7 @@ impl<M: 'static + Send + Debug> Engine<M> {
                 Event::UserEvent(command) => {
                     match command {
                         WindowCommand::RequestClose => {
-                            if let Some(internals) = &mut self.internals {
-                                internals.engine_teardown();
-                            };
+                            internals.engine_teardown();
                             *control_flow = ControlFlow::Exit
                         },
                         WindowCommand::RequestRedraw => {
@@ -96,9 +93,7 @@ impl<M: 'static + Send + Debug> Engine<M> {
                             let KeyboardInput { virtual_keycode, state, .. } = input;
                             match (virtual_keycode, state) {
                                 (Some(KeyCode::Escape), KeyState::Pressed) => {
-                                    if let Some(internals) = &mut self.internals {
-                                        internals.engine_teardown();
-                                    };
+                                    internals.engine_teardown();
                                     *control_flow = ControlFlow::Exit;
                                 },
                                 (Some(keycode), state) => {
@@ -112,71 +107,54 @@ impl<M: 'static + Send + Debug> Engine<M> {
                         },
                         WindowEvent::Focused(focused) => {
                             match focused {
-                                true => {
-                                    if self.internals.is_none() {
-                                        self.internals = Some(EngineInternals::new(&window, &app).unwrap());
-                                    }
-                                    app.on_window_state_event(WindowStateEvent::FocusGained)
-                                },
+                                true => app.on_window_state_event(WindowStateEvent::FocusGained),
                                 false => app.on_window_state_event(WindowStateEvent::FocusLost)
                             };
                         },
                         WindowEvent::CloseRequested => {
                             app.on_window_state_event(WindowStateEvent::Closing);
-                            if let Some(internals) = &mut self.internals {
-                                internals.engine_teardown();
-                            };
+                            internals.engine_teardown();
                             *control_flow = ControlFlow::Exit;
                         },
                         WindowEvent::Resized(client_area_dimensions) => {
-                            if let Some(internals) = &mut self.internals {
-                                // TODO - this recreates swapchain after first init; is it safe to not init swapchain until this?
-                                let last_known_size = internals.get_last_known_size();
-                                if last_known_size != client_area_dimensions {
-                                    let aspect_ratio = client_area_dimensions.width as f32 /
-                                        client_area_dimensions.height as f32;
-                                    app.on_render_cycle_event(
-                                        RenderCycleEvent::RecreatingSurface(aspect_ratio));
-                                    internals.recreate_surface(&window, client_area_dimensions, &app)
-                                        .unwrap();
-                                }
+                            // TODO - this recreates swapchain after first init; is it safe to not init swapchain until this?
+                            let last_known_size = internals.get_last_known_size();
+                            if last_known_size != client_area_dimensions {
+                                let aspect_ratio = client_area_dimensions.width as f32 /
+                                    client_area_dimensions.height as f32;
+                                app.on_render_cycle_event(
+                                    RenderCycleEvent::RecreatingSurface(aspect_ratio));
+                                internals.recreate_surface(&window, client_area_dimensions, &app)
+                                    .unwrap();
                             }
                         },
                         _ => {}
                     };
                 },
                 Event::MainEventsCleared => {
-                    if let Some(internals) = &mut self.internals {
-                        // TODO: v-sync?
-                        let time_passed_millis = internals.pull_time_step_millis();
-                        app.on_render_cycle_event(
-                            RenderCycleEvent::PrepareUpdate(time_passed_millis));
-                        window.request_redraw();
-                    }
+                    // TODO: v-sync?
+                    let time_passed_millis = internals.pull_time_step_millis();
+                    app.on_render_cycle_event(
+                        RenderCycleEvent::PrepareUpdate(time_passed_millis));
+                    window.request_redraw();
                 },
                 Event::RedrawRequested(_) => {
                     app.on_render_cycle_event(RenderCycleEvent::RenderingFrame);
-                    if let Some(internals) = &mut self.internals {
-                        match internals.render_frame(&app) {
-                            Ok(PresentResult::Ok) => {},
-                            Ok(PresentResult::SwapchainOutOfDate) => {
-                                if let Some(internals) = &mut self.internals {
-                                    let last_known_size = internals.get_last_known_size();
-                                    let aspect_ratio = last_known_size.width as f32 /
-                                        last_known_size.height as f32;
-                                    app.on_render_cycle_event(
-                                        RenderCycleEvent::RecreatingSurface(aspect_ratio));
-                                    internals.recreate_surface(&window, last_known_size, &app)
-                                        .unwrap();
-                                }
-                            },
-                            Err(e) => {
-                                println!("Rendering error: {:?}", e);
-                                if let Some(internals) = &mut self.internals {
-                                    internals.engine_teardown();
-                                };
-                                *control_flow = ControlFlow::Exit
-                            }
+                    match internals.render_frame(&app) {
+                        Ok(PresentResult::Ok) => {},
+                        Ok(PresentResult::SwapchainOutOfDate) => {
+                            let last_known_size = internals.get_last_known_size();
+                            let aspect_ratio = last_known_size.width as f32 /
+                                last_known_size.height as f32;
+                            app.on_render_cycle_event(
+                                RenderCycleEvent::RecreatingSurface(aspect_ratio));
+                            internals.recreate_surface(&window, last_known_size, &app)
+                                .unwrap();
+                        },
+                        Err(e) => {
+                            println!("Rendering error: {:?}", e);
+                            internals.engine_teardown();
+                            *control_flow = ControlFlow::Exit
                         }
                     }
                 },
